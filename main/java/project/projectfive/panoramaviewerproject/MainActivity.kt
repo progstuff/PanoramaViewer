b612d1d90994e03ec18978ba33914547496d89ab
Морозов Андрей <yerdnavozorom@gmail.com>
2021-08-08 16:45:51 +0300

    project created

---
+++ main/java/project/projectfive/panoramaviewerproject/MainActivity.kt
@@ -0,0 +1,19 @@
+package project.projectfive.panoramaviewerproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import project.projectfive.panoramaviewerproject.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
