8b16a3c64dfde38b13fdab0bf0906ad74e4e57a1
Морозов Андрей <yerdnavozorom@gmail.com>
2021-09-03 20:51:17 +0300

    add eye vector rotations by 2 axis

--- main/java/project/projectfive/panoramaviewerproject/OpenGLClasses/SphereRenderer/SphereRendererGL.kt
+++ main/java/project/projectfive/panoramaviewerproject/OpenGLClasses/SphereRenderer/SphereRendererGL.kt
@@ -224,14 +224,37 @@ class SphereRendererGL (private var zoom: Int) : GLSurfaceView.Renderer {
         return floatArrayOf(x,y,z)
     }
 
+    fun rotateByVector(ang_rad:Float, point:FloatArray, rotateVector:FloatArray):FloatArray{
+        var rx = rotateVector[0]
+        var ry = rotateVector[1]
+        var rz = rotateVector[2]
+        var a11 = cos(ang_rad) + (1-cos(ang_rad))*rx*rx
+        var a12 = (1-cos(ang_rad))*rx*ry - sin(ang_rad)*rz
+        var a13 = (1-cos(ang_rad))*rx*rz + sin(ang_rad)*ry
+
+        var a21 = (1-cos(ang_rad))*ry*rx + sin(ang_rad)*rz
+        var a22 = cos(ang_rad) + (1-cos(ang_rad))*ry*ry
+        var a23 = (1-cos(ang_rad))*ry*rz - sin(ang_rad)*rx
+
+        var a31 = (1-cos(ang_rad))*rz*rx - sin(ang_rad)*ry
+        var a32 = (1-cos(ang_rad))*rz*ry + sin(ang_rad)*rx
+        var a33 = cos(ang_rad) + (1-cos(ang_rad))*rz*rz
+
+        var x = point[0]*a11 + point[1]*a21 + point[2]*a31
+        var y = point[0]*a12 + point[1]*a22 + point[2]*a32
+        var z = point[0]*a13 + point[1]*a23 + point[2]*a33
+        return floatArrayOf(x,y,z)
+    }
 
     fun calculateEyeVector(alfa:Float, beta:Float, gama:Float):FloatArray{
         var a = alfa/180 * PI
         var b = beta/180 * PI
         var c = gama/180 * PI
 
-        var k = rotateByY(b.toFloat(), floatArrayOf(0f,0f,1f))
-
+        //var k = rotateByY(b.toFloat(), floatArrayOf(0f,0f,1f))
+        var k = rotateByVector(-a.toFloat(), floatArrayOf(1f,0f,0f), floatArrayOf(0f,0f,1f))
+        k = rotateByVector(b.toFloat(), k, floatArrayOf(0f,1f,0f))
+        
         //k = rotateByX(b.toFloat(), k)
         var s = sqrt(k[0]*k[0] + k[1]*k[1] + k[2]*k[2])
         k[0] = k[0]/s
